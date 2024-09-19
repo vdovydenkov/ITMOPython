6ef916e7e047d5ba12f8a63e0ee40ecb999d477f
VDovydenkov <dov@mlbox.ru>
2024-09-19 09:59:08 +0300

    task3_some_items.py теперь печатает четные значения, а не четные индексы принятого списка.

--- hw1/task3_some_items.py
+++ hw1/task3_some_items.py
@@ -1,22 +1,22 @@
-# Выборка элементов списка.
+# Выборка элементов списка
 
 import sys
 
-# Подключаем собственный модуль. Функция get_integer.
+# Подключаем собственный модуль. Функция get_integer
 from hw1_module import get_integer
 
 print("Введите последовательно любые целые числа. Для завершения, введите -1")
 
 user_integer = get_integer()
-# Если первый же ввод дал ошибку.
+# Если первый же ввод дал ошибку
 if user_integer == None:
     print("Превышено количество попыток, или произошла иная ошибка ввода.")
     sys.exit(1)
 
-# Создаем пустой список.
+# Создаем пустой список
 user_list = []
 
-# Пока ввод не закончится минус единицей или ошибкой.
+# Пока ввод не закончится минус единицей или ошибкой
 while (user_integer != -1) and (user_integer != None):
     user_list.append(user_integer)
     user_integer = get_integer()    
@@ -24,25 +24,31 @@ while (user_integer != -1) and (user_integer != None):
 print("Ввод закончен. Список чисел сформирован.")
 print("-" * 30)
 
-
-# Длина списка.
+# Длина списка
 list_length = len(user_list)
 print(f"Список содержит {list_length} элементов.")
 
-# Подсчет суммы циклом.
+# Подсчет суммы циклом
 sum_of_items = 0
 for num in user_list:
     sum_of_items += num
 print("Сумма, рассчитанная циклом: ", sum_of_items)
 
-# Подсчет суммы функцией.
+# Подсчет суммы функцией
 print("Сумма элементов, рассчитанная функцией sum(): ", sum(user_list))
 
-# Четные элементы.
-print("Все четные индексы  списка: ", user_list[1::2])
+# Выбираем четные значения
+even_numbers = []
+for item in user_list:
+    if item % 2 == 0:
+        even_numbers.append(item)
+# Проверяем, есть ли четные значения
+if len(even_numbers) > 0:
+    print("Все четные значения списка: ", even_numbers)
+else:
+    print("Четных значений в списке нет.")
 
 print("-" * 30)
-
 
 # Пауза перез завершением.
 input("Нажмите Enter для завершения программы.")
