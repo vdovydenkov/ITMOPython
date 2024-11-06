d74105d8bbec45bff6635163379ee6a9c2c4032a
VDovydenkov <dov@mlbox.ru>
2024-11-06 15:55:11 +0300

    Corrected the mistakes in the hash function.

--- lab_10.29/hash_tables.py
+++ lab_10.29/hash_tables.py
@@ -1,40 +1,34 @@
 # Лабораторная работа от 29 октября 2024 г.
 # Реализация динамической хеш-таблицы с функциями расширения и сокращения.
 
+STEP = 128  # Шаг размерности таблицы
+LOAD_FACTOR = 0.3 # Коэффициент заполнения, от которого зависит размерность
+
 class DynamicHashTable:
-    def __init__(self, capacity = 256):
+    def __init__(self, capacity=128):
         self.capacity = capacity
         self.size = 0
         self.table = [None] * self.capacity
 
-    def hash_256(self, key):
-        return hash(key) % 256
-
-    def hash_128(self, key):
-        return hash(key) % 128
-
-    def hash_function(self, key):
-        if self.capacity == 256:
-            return self.hash_256(key)
-        else:
-            return self.hash_128(key)
+    def hash(self, value):
+        return value % self.capacity
 
     def insert(self, key, value):
-        index = self.hash_function(key)
+        index = self.hash(key)
         if self.table[index] is None:
             self.table[index] = []
-        # Проверяем, существует ли уже ключ, и обновляем значение
         for i, (k, v) in enumerate(self.table[index]):
             if k == key:
                 self.table[index][i] = (key, value)
                 return
         self.table[index].append((key, value))
         self.size += 1
-        if self.size > self.capacity * 0.7:  # Увеличиваем размер, если заполненность > 70%
-            self.resize(self.capacity * 2)
+        if self.size > self.capacity * (1 - LOAD_FACTOR):
+            self.resize(self.capacity + STEP)
+        return
 
     def get(self, key):
-        index = self.hash_function(key)
+        index = self.hash(key)
         if self.table[index] is not None:
             for k, v in self.table[index]:
                 if k == key:
@@ -42,15 +36,16 @@ class DynamicHashTable:
         return None
 
     def delete(self, key):
-        index = self.hash_function(key)
+        index = self.hash(key)
         if self.table[index] is not None:
             for i, (k, v) in enumerate(self.table[index]):
                 if k == key:
                     del self.table[index][i]
                     self.size -= 1
-                    if self.size < self.capacity * 0.3:  # Уменьшаем размер, если заполненность < 30%
-                        self.resize(max(128, self.capacity // 2))
+                    if self.size < self.capacity * LOAD_FACTOR:
+                        self.resize(max(128, self.capacity - STEP))
                     return
+        return
 
     def resize(self, new_capacity):
         old_table = self.table
@@ -66,12 +61,12 @@ class DynamicHashTable:
 # Демо
 hash_table = DynamicHashTable()
 
-hash_table.insert("Заяц", 1)
-hash_table.insert("Волк", 2)
-hash_table.insert("Лиса", 3)
+hash_table.insert(1, "Заяц")
+hash_table.insert(2, "Волк")
+hash_table.insert(3, "Лиса")
 
-print(hash_table.get("Лиса"))  # Вывод: 2
+print(hash_table.get(3))
 
-hash_table.delete("Волк")
-print(hash_table.get("Волк"))
+hash_table.delete(2)
+print(hash_table.get(2))
 
